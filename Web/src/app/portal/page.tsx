9155e96ddb8e9768576585b0bfc89150537264f6
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-08-13 16:32:54 -0300

    made some changes

---
+++ Web/src/app/portal/page.tsx
@@ -0,0 +1,17 @@
+''use client';
+
+export function Index() {
+    return (
+        <main>
+            <h1>Page</h1>
+        </main>
+    );
+}use client';
+
+export function Index() {
+    return (
+        <main>
+            <h1>Page</h1>
+        </main>
+    );
+}
